506f9a958bb414e50ca3f5c0a5f4f721a98965c2
lsilvpin <lsilvpin@everis.com>
2020-08-05 10:50:15 -0300

    [ThreadStatic] Uma variável, múltiplas cópias para cada Thread

--- LearningSimpleThreads/Program.cs
+++ LearningSimpleThreads/Program.cs
@@ -6,16 +6,22 @@ namespace LearningSimpleThreads
   class Program
   {
     // Variável compartilhada para finalizar a Thread t corretamente
-    static bool stopped = false;
+    private static bool stopped = false;
+
+    [ThreadStatic] // Faz com que cada Thread tenha sua própria cópia do atributo
+    public static string Message;
 
     /// <summary>
     /// Exemplo de bloco a ser executado (Procedimento)
     /// </summary>
-    public static void ThreadProcedure()
+    public static void ThreadProcedure(Object o)
     {
+      // Ambas as Threads tem uma cópia da variável Message
+      Message = "Wooopie" + " __ Thread " + Convert.ToString(o);
+
       while (!stopped)
       {
-        Console.WriteLine("Running ...");
+        Console.WriteLine(Message + " running ...");
         Thread.Sleep(1000);
       }
     }
@@ -29,12 +35,17 @@ namespace LearningSimpleThreads
       Console.WriteLine("Aperte qualquer tecla para finalizar");
 
       // Criando uma nova Thread
-      Thread t = new Thread(new ThreadStart(ThreadProcedure));
-      t.Start(); // Ele começa a funcionar paralelamente
+      Thread t_1 = new Thread(new ParameterizedThreadStart(ThreadProcedure));
+      t_1.Start(1);
+
+      // Criando mais uma Thread
+      Thread t_2 = new Thread(new ParameterizedThreadStart(ThreadProcedure));
+      t_2.Start(2);
 
       Console.ReadKey();
       stopped = true;
-      t.Join();
+      t_1.Join();
+      t_2.Join();
     }
   }
 }
